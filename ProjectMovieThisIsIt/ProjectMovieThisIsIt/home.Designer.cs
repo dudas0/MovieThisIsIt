bbdb626af6e76fabeff46d5297caffb7dccbdb9c
dudas0 <46559945+dudas0@users.noreply.github.com>
2019-01-14 18:44:08 +0100

    Add files via upload

---
+++ ProjectMovieThisIsIt/ProjectMovieThisIsIt/home.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ProjectMovieThisIsIt
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log = new System.Windows.Forms.Button();
+            this.sign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(41, 105);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(75, 23);
+            this.log.TabIndex = 0;
+            this.log.Text = "ZALOGUJ SIĘ";
+            this.log.UseVisualStyleBackColor = true;
+            this.log.Click += new System.EventHandler(this.log_Click);
+            // 
+            // sign
+            // 
+            this.sign.Location = new System.Drawing.Point(138, 105);
+            this.sign.Name = "sign";
+            this.sign.Size = new System.Drawing.Size(122, 23);
+            this.sign.TabIndex = 1;
+            this.sign.Text = "ZAREJESTRUJ SIĘ";
+            this.sign.UseVisualStyleBackColor = true;
+            this.sign.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.sign);
+            this.Controls.Add(this.log);
+            this.Name = "home";
+            this.Text = "home";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button log;
+        private System.Windows.Forms.Button sign;
+    }
+}
